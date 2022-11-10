d095280b538a81dbe57850c85aa2e787ab8ea15a
roderickang <roderickang@hotmail.com>
2022-11-10 23:48:48 +0700

    variable type

---
+++ variableMe.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var greetings = "Hello rod";
+console.log(greetings);
---
+++ variableMe.ts
@@ -0,0 +1,10 @@
+let greetings : string = "Hello rod";
+
+let mynum = 6
+
+greetings.toLowerCase()
+
+console.log(greetings);
+
+
+export {}
